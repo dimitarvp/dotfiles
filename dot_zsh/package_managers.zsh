476c05da93cb7fa72f42759cc3e98f7c44e52603
Dimitar Panayotov <mitko.p@gmail.com>
2023-05-25 20:49:55 +0300

    add paths for software installed through package managers

---
+++ dot_zsh/package_managers.zsh
@@ -0,0 +1,17 @@
+# Programs installed through misc. package managers (only LunarVim, it seems) 
+export PATH=$HOME/.local/bin:$PATH 
+
+# Homebrew: installed programs.
+export PATH=/usr/local/sbin:$PATH
+
+# Homebrew: `curl`
+export PATH="/usr/local/opt/curl/bin:$PATH"
+
+# Homebrew: `mysql-client`
+export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+
+# Homebrew: `sqlite`
+export PATH="/usr/local/opt/sqlite/bin:$PATH"
+
+# Homebrew: OpenSSL
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
