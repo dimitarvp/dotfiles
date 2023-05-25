3f755bc3d7969dae43c7bb6261ef0eadc607aadd
Dimitar Panayotov <mitko.p@gmail.com>
2023-05-25 16:49:50 +0300

    add GNU paths and options

---
+++ gnu.zsh
@@ -0,0 +1,12 @@
+export PATH=/usr/local/opt/bison/bin:$PATH # GNU bison
+export PATH=/usr/local/opt/texinfo/bin:$PATH # GNU texinfo
+export PATH=/usr/local/opt/gnu-tar/libexec/gnubin:$PATH # GNU tar
+
+export LDFLAGS="-L/usr/local/opt/bison/lib"
+
+# To use all GNU core utilities, uncomment these:
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}"
+
+# GNU grep.
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
