18c68a07dfdc14f9c8a5df4e63782dd6df18b1aa
Dimitar Panayotov <mitko.p@gmail.com>
2023-05-25 21:25:29 +0300

    unify p10k

--- dot_zsh/p10k.zsh
+++ dot_zsh/p10k.zsh
@@ -11,3 +11,6 @@ fi
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 POWERLEVEL10K_MODE='nerdfont-complete'
 ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
