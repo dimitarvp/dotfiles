81bb2714eeb8389d20fdd13005f600bb691df1ce
Dimitar Panayotov <mitko.p@gmail.com>
2023-05-25 20:33:42 +0300

    add personal executable paths

---
+++ dot_zsh/paths.zsh
@@ -0,0 +1,3 @@
+export PATH=$PATH:$HOME/OneDrive/vrobot.programs # Cloud-synced scripts
+export PATH=$PATH:$HOME/bin # Programs installed outside package managers
+export PATH=$PATH:$HOME/scripts # Local scripts
