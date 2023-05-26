df361a24950a2445d650d0bb874077eaa3b74ce5
Dimitar Panayotov <mitko.p@gmail.com>
2023-05-26 23:30:07 +0300

    make certain commands Mac-only

--- dot_zsh/package_managers.zsh
+++ dot_zsh/package_managers.zsh
@@ -1,17 +1,21 @@
+system_type=$(uname -s)
+
 # Programs installed through misc. package managers (only LunarVim, it seems) 
 export PATH=$HOME/.local/bin:$PATH 
 
-# Homebrew: installed programs.
-export PATH=/usr/local/sbin:$PATH
+if [ "$system_type" = "Darwin" ]; then
+  # Homebrew: installed programs.
+  export PATH=/usr/local/sbin:$PATH
 
-# Homebrew: `curl`
-export PATH="/usr/local/opt/curl/bin:$PATH"
+  # Homebrew: `curl`
+  export PATH="/usr/local/opt/curl/bin:$PATH"
 
-# Homebrew: `mysql-client`
-export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+  # Homebrew: `mysql-client`
+  export PATH="/usr/local/opt/mysql-client/bin:$PATH"
 
-# Homebrew: `sqlite`
-export PATH="/usr/local/opt/sqlite/bin:$PATH"
+  # Homebrew: `sqlite`
+  export PATH="/usr/local/opt/sqlite/bin:$PATH"
 
-# Homebrew: OpenSSL
-export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+  # Homebrew: OpenSSL
+  export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+fi
