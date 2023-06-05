404638fd6995d6b6a084dc9913edde0dc96d1a24
Dimitar Panayotov <mitko.p@gmail.com>
2023-06-05 03:58:24 +0300

    configure `fzf-tab` and break apart config further

--- dot_zsh/zsh.zsh
+++ dot_zsh/zsh.zsh
@@ -1,23 +1,3 @@
-
-#setopt BANG_HIST                 # Treat the '!' character specially during expansion.
-setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
-setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
-setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
-setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
-setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
-setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
-#setopt HIST_BEEP                 # Beep when accessing nonexistent history.
-
-# History size location and limits.
-export HISTFILE="$HOME/.zsh_history"
-export HISTSIZE=10000000
-export SAVEHIST=$HISTSIZE
-
 # disable ctrl+s, ctrl+q
 setopt no_flow_control
 
@@ -34,8 +14,8 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LC_TIME=en_US.UTF-8
 
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+autoload -Uz compinit
+compinit
 
 fpath+=${ZDOTDIR:-$HOME}/.zsh_functions
 
