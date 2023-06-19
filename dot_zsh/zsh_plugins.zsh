6d47543f1025d6f77d766773066426df2d9febcd
Dimitar Panayotov <mitko.p@gmail.com>
2023-06-19 16:52:13 +0300

    vary ZSH plugins path depending on OS

--- dot_zsh/zsh_plugins.zsh
+++ dot_zsh/zsh_plugins.zsh
@@ -1,3 +1,14 @@
 source $ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+system_type=$(uname -s)
+
+if [ "$system_type" = "Darwin" ]; then
+  declare -r zsh_plugins="/usr/local/share"
+elif [ "$system_type" = "Linux" ]; then
+  declare -r zsh_plugins="/usr/share/zsh/plugins"
+else
+  echo "UNKNOWN SYSTEM: $(uname -a)"
+fi
+
+source $zsh_plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $zsh_plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
