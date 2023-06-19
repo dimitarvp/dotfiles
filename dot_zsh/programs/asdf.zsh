73901eea70d7381fe099e99db9242c2fb1282c92
Dimitar Panayotov <mitko.p@gmail.com>
2023-06-19 17:33:27 +0300

    vary ASDF init depending on OS

--- dot_zsh/programs/asdf.zsh
+++ dot_zsh/programs/asdf.zsh
@@ -1,2 +1,12 @@
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
+system_type=$(uname -s)
+
+if [ "$system_type" = "Darwin" ]; then
+  export ASDF_DIR=$(brew --prefix asdf)/libexec
+  . $(brew --prefix asdf)/libexec/asdf.sh
+  . $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
+elif [ "$system_type" = "Linux" ]; then
+  . /opt/asdf-vm/asdf.sh
+else
+  echo "UNKNOWN SYSTEM: $(uname -a)"
+fi
+
