c55b7e4bfd06db84189e746a5221792615c67438
Dimitar Panayotov <mitko.p@gmail.com>
2023-12-24 13:48:00 +0200

    add GIT root and current dir functions

--- dot_zsh/programs/git.zsh
+++ dot_zsh/programs/git.zsh
@@ -5,3 +5,11 @@ git_default_branch() {
 git_current_branch() {
     git rev-parse --abbrev-ref HEAD
 }
+
+git_root() {
+  git rev-parse --show-toplevel
+}
+
+git_current_dir() {
+  realpath -m --relative-to $(git_root) .
+}
