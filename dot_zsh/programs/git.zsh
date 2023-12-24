68adb9320e6b64c01ca635c2ffae0ab27e55f2a9
Dimitar Panayotov <mitko.p@gmail.com>
2023-12-24 14:17:39 +0200

    add "choose GIT change" which also shows diff

--- dot_zsh/programs/git.zsh
+++ dot_zsh/programs/git.zsh
@@ -13,3 +13,8 @@ git_root() {
 git_current_dir() {
   realpath -m --relative-to $(git_root) .
 }
+
+git_choose_change() {
+  git status --porcelain=v2 --renames | awk '{print $NF}' | fzf --color header:italic --header 'GIT changes' --preview "git diff $@ --color=always -- {-1}"
+}
+
