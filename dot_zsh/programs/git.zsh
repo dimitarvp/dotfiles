c0aa591cba722748564327664ff517172df8cbce
Dimitar Panayotov <mitko.p@gmail.com>
2023-12-24 13:48:14 +0200

    fix indentation

--- dot_zsh/programs/git.zsh
+++ dot_zsh/programs/git.zsh
@@ -1,9 +1,9 @@
 git_default_branch() {
-    (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@') 2>/dev/null
+  (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@') 2>/dev/null
 }
 
 git_current_branch() {
-    git rev-parse --abbrev-ref HEAD
+  git rev-parse --abbrev-ref HEAD
 }
 
 git_root() {
