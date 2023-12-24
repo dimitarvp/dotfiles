1decb367cd75c5199de514a34efad456f8ab9037
Dimitar Panayotov <mitko.p@gmail.com>
2023-12-24 14:38:47 +0200

    remove warning about read-only shell variables

--- dot_zsh/zsh_plugins.zsh
+++ dot_zsh/zsh_plugins.zsh
@@ -3,9 +3,9 @@ source $ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh
 system_type=$(uname -s)
 
 if [ "$system_type" = "Darwin" ]; then
-  declare -r zsh_plugins="/usr/local/share"
+  declare +r zsh_plugins="/usr/local/share"
 elif [ "$system_type" = "Linux" ]; then
-  declare -r zsh_plugins="/usr/share/zsh/plugins"
+  declare +r zsh_plugins="/usr/share/zsh/plugins"
 else
   echo "UNKNOWN SYSTEM: $(uname -a)"
 fi
