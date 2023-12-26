1f80859ae863e0beb82a305980e2fc58cba379c7
Dimitar Panayotov <mitko.p@gmail.com>
2023-12-26 12:06:03 +0200

    make zplug loadable on Linux

--- dot_zsh/zplug.zsh
+++ dot_zsh/zplug.zsh
@@ -1,4 +1,12 @@
-source $(brew --prefix zplug)/init.zsh
+
+if [ "$system_type" = "Darwin" ]; then
+  source $(brew --prefix zplug)/init.zsh
+elif [ "$system_type" = "Linux" ]; then
+  # More concretely, this works on Arch / Manjaro. Unclear for other Linux flavors.
+  source /usr/share/zsh/scripts/zplug/init.zsh
+else
+  echo "zplug: UNKNOWN SYSTEM: $(uname -a)"
+fi
 
 zplug "bigH/git-fuzzy", as:command, use:"bin/git-fuzzy"
 
