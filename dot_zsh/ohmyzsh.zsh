2728df236f23fb36dfa51a6f1558dc70725ca076
Dimitar Panayotov <mitko.p@gmail.com>
2024-01-05 03:00:04 +0200

    add `colorize` plugin and use `chroma` for it

--- dot_zsh/ohmyzsh.zsh
+++ dot_zsh/ohmyzsh.zsh
@@ -48,7 +48,10 @@ COMPLETION_WAITING_DOTS="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 
-plugins=(colored-man-pages fd fzf git httpie mix ripgrep rsync rust z)
+plugins=(colored-man-pages colorize fd fzf git httpie mix ripgrep rsync rust z)
+
+# For the `colorize` ohmyzsh plugin.
+ZSH_COLORIZE_TOOL=chroma
 
 # https://stackoverflow.com/a/71271754/285154
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
