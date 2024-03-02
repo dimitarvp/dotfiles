05204551639bdbe498461f298ffaf6b5b65eb7a0
Dimitar Panayotov <mitko.p@gmail.com>
2024-03-02 02:11:39 +0200

    remove OneDrive-synced scripts

--- dot_zsh/paths.zsh
+++ dot_zsh/paths.zsh
@@ -1,3 +1,2 @@
-export PATH=$PATH:$HOME/OneDrive/vrobot.programs # Cloud-synced scripts
 export PATH=$PATH:$HOME/bin # Programs installed outside package managers
 export PATH=$PATH:$HOME/scripts # Local scripts
