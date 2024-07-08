ab89fae9d2383863c9f098b640dfdbc6c5308289
Dimitar Panayotov <mitko.p@gmail.com>
2024-07-08 13:28:13 +0300

    ignore ODBC when compiling Erlang

--- dot_zsh/languages/erlang.zsh
+++ dot_zsh/languages/erlang.zsh
@@ -1,7 +1,10 @@
-# Erlang installed through `asdf`: always build docs.
+# Always build docs.
 export KERL_BUILD_DOCS=yes
 export KERL_INSTALL_MANPAGES=yes
 export KERL_INSTALL_HTMLDOCS=yes
+
+# We don't care about ODBC.
+export KERL_CONFIGURE_OPTIONS='--without-odbc'
 
 # Enable Erlang/Elixir REPL history
 export ERL_AFLAGS="-kernel shell_history enabled"
