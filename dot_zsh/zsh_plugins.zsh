70c8bde7a7953159919bbe2191490670b123ec46
Dimitar Panayotov <mitko.p@gmail.com>
2025-05-16 01:52:05 +0300

    add one more ZSH plugins directory

--- dot_zsh/zsh_plugins.zsh
+++ dot_zsh/zsh_plugins.zsh
@@ -1,7 +1,11 @@
 # source $ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh
 
 if [ "$system_type" = "Darwin" ]; then
-  declare +r zsh_plugins="/usr/local/share"
+  if [ -d "/usr/local/share" ]; then
+    declare +r zsh_plugins="/usr/local/share"
+  elif [ -d "/opt/homebrew/Cellar" ]; then
+    declare +r zsh_plugins="/opt/homebrew/Cellar"
+  fi
 elif [ "$system_type" = "Linux" ]; then
   declare +r zsh_plugins="/usr/share/zsh/plugins"
 else
