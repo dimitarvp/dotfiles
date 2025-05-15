4b114a5b66fdb3eb6275690d674761cd0edcf2a9
Dimitar Panayotov <mitko.p@gmail.com>
2025-05-16 01:43:57 +0300

    remove removed plugins (they are used by default now)

--- dot_zsh/ohmyzsh.zsh
+++ dot_zsh/ohmyzsh.zsh
@@ -48,7 +48,7 @@ COMPLETION_WAITING_DOTS="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 
-plugins=(colored-man-pages colorize fd fzf httpie mix ripgrep rsync rust z)
+plugins=(colored-man-pages colorize fzf httpie mix rsync rust z)
 
 # For the `colorize` ohmyzsh plugin.
 export ZSH_COLORIZE_TOOL=chroma
