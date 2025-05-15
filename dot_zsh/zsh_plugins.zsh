a39a8808f31dda532b6990bf4f28b130770a9482
Dimitar Panayotov <mitko.p@gmail.com>
2025-05-16 02:07:48 +0300

    attempt to use the official OMZ plugins dir

--- dot_zsh/zsh_plugins.zsh
+++ dot_zsh/zsh_plugins.zsh
@@ -1,11 +1,12 @@
 # source $ZSH_CUSTOM/plugins/fzf-tab/fzf-tab.plugin.zsh
 
 if [ "$system_type" = "Darwin" ]; then
-  if [ -d "/usr/local/share" ]; then
-    declare +r zsh_plugins="/usr/local/share"
-  elif [ -d "/opt/homebrew/Cellar" ]; then
-    declare +r zsh_plugins="/opt/homebrew/Cellar"
-  fi
+  # if [ -d "/usr/local/share" ]; then
+  #   declare +r zsh_plugins="/usr/local/share"
+  # elif [ -d "/opt/homebrew/Cellar" ]; then
+  #   declare +r zsh_plugins="/opt/homebrew/Cellar"
+  # fi
+  declare -r zsh_plugins="$ZSH_CUSTOM/plugins"
 elif [ "$system_type" = "Linux" ]; then
   declare +r zsh_plugins="/usr/share/zsh/plugins"
 else
