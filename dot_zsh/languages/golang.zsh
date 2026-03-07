886e79e925331dee8692c70b99703918af778454
Dimitar Panayotov <mitko.p@gmail.com>
2026-03-08 01:29:32 +0200

    set GOBIN to ~/go/bin for mise-managed go
    mise sets GOBIN to its internal install dir, which puts
    go install binaries where they're not on PATH. override
    GOBIN in both zsh config and gup_converge.

--- dot_zsh/languages/golang.zsh
+++ dot_zsh/languages/golang.zsh
@@ -1,1 +1,4 @@
 # Go is managed by mise; GOPATH defaults to ~/go
+# Override GOBIN so `go install` puts binaries in ~/go/bin (on PATH)
+# instead of mise's internal bin dir
+export GOBIN="$HOME/go/bin"
