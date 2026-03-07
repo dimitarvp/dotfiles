b5634e885e3e7ffe8d0f8fd299368fd013354083
Dimitar Panayotov <mitko.p@gmail.com>
2026-03-07 20:24:08 +0200

    vendor zsh plugins, remove platform-specific paths
    vendor fzf-tab v1.2.0, zsh-autosuggestions v0.7.1,
    and zsh-syntax-highlighting 0.8.0 into dot_zsh/vendor/.
    eliminates dependency on OMZ (macOS) and pacman (Linux)
    for these three plugins.

--- dot_zsh/zsh_plugins.zsh
+++ dot_zsh/zsh_plugins.zsh
@@ -1,22 +1,9 @@
-if is_macos; then
-  zsh_plugins="$HOME/.oh-my-zsh/custom/plugins"
-elif is_linux; then
-  zsh_plugins="/usr/share/zsh/plugins"
-else
-  echo "UNKNOWN SYSTEM: $(uname -a)"
-fi
-
 source $HOME/.zsh/vendor/async.zsh
-
-if [[ -f $zsh_plugins/fzf-tab/fzf-tab.plugin.zsh ]]; then
-  source $zsh_plugins/fzf-tab/fzf-tab.plugin.zsh
-elif [[ -f $zsh_plugins/fzf-tab-git/fzf-tab.plugin.zsh ]]; then
-  source $zsh_plugins/fzf-tab-git/fzf-tab.plugin.zsh
-fi
+source $HOME/.zsh/vendor/fzf-tab/fzf-tab.zsh
 
 export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE='32'
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 
-source $zsh_plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $zsh_plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/vendor/zsh-autosuggestions.zsh
+source $HOME/.zsh/vendor/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
