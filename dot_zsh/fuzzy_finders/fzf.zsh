96f0c07846e27f682a248ef70d46c53d8880a086
Dimitar Panayotov <mitko.p@gmail.com>
2026-03-07 23:17:35 +0200

    add atuin as shell history manager
    replace fzf Ctrl-R with atuin TUI. source atuin after
    fzf so its keybinding wins. remove dead FZF_CTRL_R_OPTS.
    add chezmoi-managed config and cargo crates entry.

--- dot_zsh/fuzzy_finders/fzf.zsh
+++ dot_zsh/fuzzy_finders/fzf.zsh
@@ -9,19 +9,6 @@ export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
-if is_macos; then
-  _fzf_clip='pbcopy'
-else
-  _fzf_clip='wl-copy'
-fi
-
-export FZF_CTRL_R_OPTS="
-  --preview 'echo {}' --preview-window up:3:hidden:wrap
-  --bind 'ctrl-/:toggle-preview'
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | $_fzf_clip)+abort'
-  --color header:italic
-  --header 'Press CTRL-Y to copy command into clipboard'"
-
 export FZF_ALT_C_COMMAND='fd -t d'
 export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
 
