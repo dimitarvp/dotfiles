18ba1c8d2e0c4fc0bc40b3aaf5c7f13d292b56e1
Dimitar Panayotov <mitko.p@gmail.com>
2026-03-22 15:11:48 +0200

    run shfmt on all shell scripts

--- scripts/executable_download_site.sh
+++ scripts/executable_download_site.sh
@@ -1,25 +1,25 @@
 #! /bin/sh
 
 wget \
-    --execute robots=off \
-    --recursive \
-    --level 64 \
-    --timestamping \
-    --continue \
-    --page-requisites \
-    --adjust-extension \
-    --convert-links \
-    --restrict-file-names=ascii,lowercase \
-    --no-remove-listing \
-    --no-parent \
-    --tries 5 \
-    --timeout 30 \
-    --retry-connrefused \
-    --xattr \
-    --no-host-directories \
-    --span-hosts \
-    --verbose \
-    "$@"
+	--execute robots=off \
+	--recursive \
+	--level 64 \
+	--timestamping \
+	--continue \
+	--page-requisites \
+	--adjust-extension \
+	--convert-links \
+	--restrict-file-names=ascii,lowercase \
+	--no-remove-listing \
+	--no-parent \
+	--tries 5 \
+	--timeout 30 \
+	--retry-connrefused \
+	--xattr \
+	--no-host-directories \
+	--span-hosts \
+	--verbose \
+	"$@"
 
 #    --directory-prefix .
 #    --limit-rate 78.5k
@@ -29,4 +29,4 @@ wget \
 #    --http-user=USERNAME
 #    --http-password=PASSWORD
 #    --domains website.org \
-    #    www.website.org/tutorials/html/
+#    www.website.org/tutorials/html/
