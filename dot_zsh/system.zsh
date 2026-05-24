fdde698a88d7a747aaa122384315f8d785aa41be
Dimitar Panayotov <mitko.p@gmail.com>
2026-05-24 05:13:34 +0300

    set COLORTERM=truecolor in shell init
    terminals set COLORTERM locally but ssh doesn't forward it, leaving
    headless and remote shells without truecolor advertisement. pin it
    in system.zsh so all machines agree.

--- dot_zsh/system.zsh
+++ dot_zsh/system.zsh
@@ -3,4 +3,9 @@ ulimit -n 98304
 export EDITOR=nvim
 export VISUAL=nvim
 
+# Terminals set this locally but SSH doesn't forward it; pin it so headless
+# / remote shells (s1, robogamer) advertise truecolor support to apps too.
+: ${COLORTERM:=truecolor}
+export COLORTERM
+
 WORDCHARS='*?_[]~=&;!#$%^(){}<>'
